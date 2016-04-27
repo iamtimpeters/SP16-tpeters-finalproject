8b2d5385c486494e9ef29ae8d70dcfefafca1fda
Tim Peters <iamtimpeters@gmail.com>
2016-04-26 20:38:15 -0500

    Update products-list.js

--- js/products-list.js
+++ js/products-list.js
@@ -53,7 +53,7 @@ $(document).ready(function(){
     		self.products = ko.observableArray(productsData);
 
             self.productDetailLink = function(products){
-                var localSource = "file:///Users/Tim/Documents/Parkland%20classes/CSC175/SP16-tpeters/final-project-resources-master/site-starter/product-details.html";    
+                var localSource = "http://parkland-csc175.github.io/SP16-tpeters-finalproject/product-details.html";    
                 var productSku = products.sku;
                 localSource += "#" + productSku
                 console.log(document.location.hash);
